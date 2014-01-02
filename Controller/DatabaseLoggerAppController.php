3694976a89a0c1d69e00170715d888bfae1be03d
Angelito M. Goulart <angelito@bsd.com.br>
2014-01-02 10:36:57 -0200

    Delete DatabaseLoggerAppController.php

--- Controller/DatabaseLoggerAppController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DatabaseLoggerAppController extends AppController {
-	protected function dataToNamed($key = 'Search'){
-  	$params = is_array($this->request->params['named']) ? $this->request->params['named'] : array();
-  	$data = isset($this->request->data[$key]) ? $this->request->data[$key] : array();
-  	$this->request->params['named'] = array_merge($data, $params);
-  }
-}
